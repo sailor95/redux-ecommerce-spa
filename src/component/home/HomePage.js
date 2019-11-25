bfcf21ba5bd40ba2a3b03b6cdadbc1c542565947
David Huang <bugman195@gmail.com>
2019-11-25 22:41:11 +0800

    Connect Home page with Redux
    Summary:
    
    - Transform Home page to class component
    - Connect Home with Store and Action Creators
    - Get all items on mount

--- src/component/home/HomePage.js
+++ src/component/home/HomePage.js
@@ -1,16 +1,36 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import { connect } from 'react-redux';
 
 import MyNavbar from '../navbar/MyNavbar';
 import ProductList from '../product-list/ProductList';
+import actions from '../../store/items/actions';
 
-const HomePage = () => {
-  return (
-    <Container>
-      <MyNavbar />
-      <ProductList />
-    </Container>
-  );
+class HomePage extends React.Component {
+  componentDidMount() {
+    this.props.getAllItems();
+    this.props.getItemById(1); // NOTE: Testing purpose, delete in the future
+    this.props.getAllCategories(); // NOTE: Testing purpose, delete in the future
+  }
+
+  render() {
+    return (
+      <Container>
+        <MyNavbar />
+        <ProductList items={this.props.items.items} />
+      </Container>
+    );
+  }
 };
 
-export default HomePage;
+const mapStateToProps = state => ({
+  items: state.items
+});
+
+const mapDispatchToProps = {
+  getAllItems: actions.getAllItems,
+  getItemById: actions.getItemById,
+  getAllCategories: actions.getAllCategories
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
