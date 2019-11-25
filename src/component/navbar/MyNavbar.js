5b7c14c173d985212193093ba4a455705f1e1b95
David Huang <david.huang@getac.com.tw>
2019-11-25 13:59:03 +0800

    Create Navbar component

---
+++ src/component/navbar/MyNavbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+
+const MyNavbar = () => (
+  <Row>
+    <Col xs={12} className="d-flex justify-content-center border">
+      <div>
+        My Navbar
+      </div>
+    </Col>
+  </Row>
+);
+
+export default MyNavbar;
