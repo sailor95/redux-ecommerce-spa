b3f09573d28315b6f37cbd95798babe46fe3edb6
David Huang <david.huang@getac.com.tw>
2019-11-25 14:25:36 +0800

    Create basic Redux boilerplate & products reducer

--- src/component/app/App.js
+++ src/component/app/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { createBrowserHistory } from 'history';
 import { Router, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import HomePage from '../home/HomePage';
+import configStore from '../../store/configStore';
 
 const history = createBrowserHistory();
+const store = configStore({});
 
 const App = () => (
-  <Router history={history}>
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-    </Switch>
-  </Router>
+  <Provider store={store}>
+    <Router history={history}>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
+    </Router>
+  </Provider>
 );
 
 export default App;
---
+++ src/store/rootReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import products from './products/reducer';
+
+const rootReducer = combineReducers({
+  products
+});
+
+export default rootReducer;
