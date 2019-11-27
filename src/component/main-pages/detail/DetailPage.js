98e511e8dd816eb2424f4d0fa0b8cfd537012bc4
David Huang <david.huang@getac.com.tw>
2019-11-27 17:07:35 +0800

    Polish Detail page CSS
    Summary:
    
    - Create Like button
    - Create Price fixed footer
    - Update CSS to fix requirement

--- src/component/main-pages/detail/DetailPage.js
+++ src/component/main-pages/detail/DetailPage.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Container, Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
 
+import classes from './detailPage.module.css';
 import actions from '../../../store/items/actions';
 import NotFoundPage from '../not-found/NotFoundPage';
 import DetailPageNavbar from './detail-page-navbar/DetailPageNavbar';
@@ -31,12 +34,33 @@ class DetailPage extends React.Component {
       <Container>
         <DetailPageNavbar title={name} />
         <Row>
-          <Col>
+          <Col
+            xs={12}
+            className={`d-flex justify-content-center ${classes.imgBackground}`}
+          >
             <img width="50%" src={image} />
-            <h5>Likes: {like_count}</h5>
-            <h5>Price: ${price}</h5>
-            <h5>Shipping Fee: {shipping_fee}</h5>
+          </Col>
+          <Col xs={12}>
+            <h1>{name}</h1>
+          </Col>
+          <Col xs={12}>
+            <div className={`${classes.likeBtn} d-flex mt-3 mb-3`}>
+              <FontAwesomeIcon
+                icon={faHeart}
+                size="2x"
+                className={`${classes.likeBtnLikeIcon} my-auto ml-4`}
+              />
+              <div className={`${classes.likeBtnLikeCount} ml-3`}>
+                {like_count}
+              </div>
+            </div>
+          </Col>
+          <Col xs={12} className={classes.descriptionContent}>
             <p>Description: {description}</p>
+          </Col>
+          <Col xs={12} className={`${classes.floatBar} d-flex`}>
+            <h5 className="my-auto ml-4">Price: ${price}</h5>
+            <h5 className="my-auto ml-4">Shipping Fee: {shipping_fee}</h5>
           </Col>
         </Row>
       </Container>
