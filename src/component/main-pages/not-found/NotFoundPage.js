1888f590771f01f21228dcebe838236afe75dbe3
David Huang <bugman195@gmail.com>
2019-11-28 01:12:14 +0800

    Update NotFoundPage

--- src/component/main-pages/not-found/NotFoundPage.js
+++ src/component/main-pages/not-found/NotFoundPage.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+
+import DetailPageNavbar from '../../navbar/DetailPageNavbar';
 
 const NotFoundPage = ({ id }) => (
   <Container>
-    <Row>
-      <Col>
-        <Link to="/">Back to home</Link>
-        <h1>Detail Page</h1>
-        <div>Product Id: {id}</div>
-        <hr />
-        <h3>Product Not Found</h3>
-      </Col>
-    </Row>
+    <DetailPageNavbar title={`Product Id: ${id}`} />
+    <h1>Product Not Found</h1>
+    <h3>Product Id '{id}' Not Found</h3>
   </Container>
 );
+
+NotFoundPage.propTypes = {
+  id: PropTypes.string.isRequired
+};
 
 export default NotFoundPage;
